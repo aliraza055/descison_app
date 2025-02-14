4ae2ddeb7e63d9cb4237691c90dd8c9387e30316
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-02-14 12:15:23 +0500

    initilize varibles

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -8,11 +8,43 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _keyform=GlobalKey<FormState>();
+  var gmail='';
+  var password='';
+  var name='';
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
+        title:const Text("login page"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+        const  SizedBox(
+            height: 150,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: "Enter Gmail",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8)
+              )
+            ),
+          ),
+        const  SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: "Enter Password",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8)
+              )
+            ),
+          )
+        ],
       ),
     );
   }
