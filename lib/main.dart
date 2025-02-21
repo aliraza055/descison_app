584938537e962d8307e72118aa8cdc18526a1963
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-02-21 21:29:21 +0500

    false

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(primaryColor: AppColors().primary),
       home: FutureBuilder<User?>(
